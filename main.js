f776115df62bfeffcf17a005d95dce76d5193be5
John Nowinsky <armthepit@armthepit.com>
2016-10-20 07:29:02 -0500

    added requires inquirer,game,letter,word to main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+// Load the NPM Package Inquirer
+var inquirer = require('inquirer');
+// Load game.js
+var game = require('./game.js');
+// Load letter.js
+var letter = require('./letter.js');
+// Load word.js
+var word = require('./word.js');
+
--- word.js
+++
@@ -1,7 +0,0 @@
-// Requires 
-// Game.js require is used to pull in the random word from the Random Words node package.
-var game = require('./game.js')
-
-// Call the random word and save to the variable
-var word = game.word;
-console.log(word);
